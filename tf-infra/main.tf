db6060940fbdc802108e5a322675d0f62e6eeafb
sry1 <steve.roy99@yahoo.com>
2025-01-26 16:17:41 +0300

    fix: sg ingress

--- tf-infra/main.tf
+++ tf-infra/main.tf
@@ -11,6 +11,7 @@ resource "aws_instance" "web" {
   ami           = "ami-04b4f1a9cf54c11d0"
   instance_type = "t2.micro"
   key_name      = aws_key_pair.devops_key.key_name
+  security_groups = [aws_security_group.pass_sg.name]
 
   tags = {
     Name = "password-gen"
@@ -52,6 +53,20 @@ resource "aws_security_group" "pass_sg" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+   from_port   = 22
+   to_port     = 22
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
